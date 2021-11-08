007c749812c2f7719406e98e71507afa8fa30b32
FireMaskterK <20838718+FireMasterK@users.noreply.github.com>
2021-11-08 02:10:14 +0000

    Build more modules.

--- Dockerfile
+++ Dockerfile
@@ -13,8 +13,7 @@ RUN apk add --no-cache gcc \
     patch \
     rust \
     cargo \
-    g++ \
-    nginx
+    g++
 
 COPY build.sh .
 
--- build.sh
+++ build.sh
@@ -8,9 +8,9 @@ mv nginx-1.16.1 nginx
 cd nginx
 patch -p01 <../quiche/extras/nginx/nginx-1.16.patch
 ./configure \
-	--prefix=/var/lib/nginx \
+	--prefix=/etc/nginx \
 	--sbin-path=/usr/sbin/nginx \
-	--modules-path=/var/lib/nginx/modules \
+	--modules-path=/etc/nginx/modules \
 	--conf-path=/etc/nginx/nginx.conf \
 	--pid-path=/run/nginx/nginx.pid \
 	--lock-path=/run/nginx/nginx.lock \
@@ -19,6 +19,11 @@ patch -p01 <../quiche/extras/nginx/nginx-1.16.patch
 	--with-http_ssl_module \
 	--with-http_v2_module \
 	--with-http_v3_module \
+	--with-file-aio \
+	--with-http_sub_module \
+	--with-threads \
+	--with-http_gunzip_module \
 	--with-openssl=../quiche/deps/boringssl \
 	--with-quiche=../quiche
 make
+make install
