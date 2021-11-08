b14c20289702b8cf772bbaa28fbd5ea0565077b7
FireMaskterK <20838718+FireMasterK@users.noreply.github.com>
2021-11-08 02:31:34 +0000

    Change some paths.

--- build.sh
+++ build.sh
@@ -14,6 +14,13 @@ patch -p01 <../quiche/extras/nginx/nginx-1.16.patch
 	--conf-path=/etc/nginx/nginx.conf \
 	--pid-path=/run/nginx/nginx.pid \
 	--lock-path=/run/nginx/nginx.lock \
+	--error-log-path=/var/log/nginx/error.log \
+	--http-log-path=/var/log/nginx/access.log \
+	--http-client-body-temp-path=/var/lib/nginx/tmp/client_body \
+	--http-proxy-temp-path=/var/lib/nginx/tmp/proxy \
+	--http-fastcgi-temp-path=/var/lib/nginx/tmp/fastcgi \
+	--http-uwsgi-temp-path=/var/lib/nginx/tmp/uwsgi \
+	--http-scgi-temp-path=/var/lib/nginx/tmp/scgi \
 	--user=nginx \
 	--group=nginx \
 	--with-http_ssl_module \
@@ -23,6 +30,8 @@ patch -p01 <../quiche/extras/nginx/nginx-1.16.patch
 	--with-http_sub_module \
 	--with-threads \
 	--with-http_gunzip_module \
+	--with-http_addition_module \
+	--with-stream_ssl_preread_module \
 	--with-openssl=../quiche/deps/boringssl \
 	--with-quiche=../quiche
 make
